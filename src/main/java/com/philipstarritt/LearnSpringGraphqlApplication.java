3295efa838fa419d1a59ececf14b0c75d1a41610
Philip Starritt <philipstarritt@Philips-MacBook-Pro.local>
2022-12-02 11:09:09 +0100

    Learn spring-graphql base

---
+++ src/main/java/com/philipstarritt/LearnSpringGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.philipstarritt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/philipstarritt/LearnSpringGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philipstarritt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
